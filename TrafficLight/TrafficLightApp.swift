bb34dfbe52f1a11d890e70438e35bb4d19a224eb
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-04-19 16:22:28 -0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Ilya Zemskov on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
